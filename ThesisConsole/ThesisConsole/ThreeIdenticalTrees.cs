3329c94af24d5c7e58adf648155138ee3cf8d6c0
Jeroen <jer_vannevel@hotmail.com>
2015-12-20 14:52:51 +0100

    init commit: linqalloc

---
+++ ThesisConsole/ThesisConsole/ThreeIdenticalTrees.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThesisConsole
+{
+    internal class ThreeIdenticalTrees
+    {
+        private const int y = 5;
+        static void Main(string[] args)
+        {
+            const string tree1 = @"
+using System;
+using System.Text;
+
+class MyClass 
+{
+    void MyMethod()
+    {
+        int result = 2 + 2;
+        Console.WriteLine(result);
+    }
+}";
+
+            //            const string tree2 = @"
+            //using System;
+            //using System.Text;
+
+            //class MyClass 
+            //{
+            //    void MyMethod()
+            //    {
+            //        int result = 2 + 2;
+            //        Console.WriteLine(result);
+            //    }
+            //}";
+
+            //            const string tree3 = @"
+            //using System;
+            //using System.Text;
+
+            //class MyClass 
+            //{
+            //    void MyMethod()
+            //    {
+            //        int result = 2 + 2;
+            //        Console.WriteLine(result);
+            //    }
+            //}";
+            //        }
+
+        }
+}
---
+++ ThesisConsole/ThesisConsole/linqalloc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThesisConsole
+{
+    internal class LinqAlloc
+    {
+        private static void Main2(string[] args)
+        {
+            // Setting baseline snapshot
+            var list1 = new List<int> {4862, 6541, 7841};
+            var list2 = new List<int>(list1.Count);
+            var list3 = new List<int>(list1.Count);
+
+            // First snapshot: LINQ usage
+            list2.AddRange(list1.Where(item => item > 5000 && item < 7000));
+
+            // Second snapshot: foreach-loop
+            foreach (var item in list1)
+            {
+                if (item > 5000 && item < 7000)
+                {
+                    list3.Add(item);
+                }
+            }
+
+            // End gather
+            Console.Read();
+        }
+    }
+}
