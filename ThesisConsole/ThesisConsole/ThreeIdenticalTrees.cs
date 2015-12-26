e1c0237a010c2203d2de1e4c3995a4b66ae608c6
Jeroen <jer_vannevel@hotmail.com>
2015-12-26 21:15:54 +0100

    added analyzer project

--- ThesisConsole/ThesisConsole/ThreeIdenticalTrees.cs
+++ ThesisConsole/ThesisConsole/ThreeIdenticalTrees.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+
+#pragma warning disable 219
 
 namespace ThesisConsole
 {
     internal class ThreeIdenticalTrees
     {
-        private const int y = 5;
-        static void Main(string[] args)
-        {
-            const string tree1 = @"
+        public const string tree1 = @"
 using System;
 using System.Text;
 
@@ -23,33 +23,10 @@ class MyClass
         Console.WriteLine(result);
     }
 }";
-
-            //            const string tree2 = @"
-            //using System;
-            //using System.Text;
-
-            //class MyClass 
-            //{
-            //    void MyMethod()
-            //    {
-            //        int result = 2 + 2;
-            //        Console.WriteLine(result);
-            //    }
-            //}";
-
-            //            const string tree3 = @"
-            //using System;
-            //using System.Text;
-
-            //class MyClass 
-            //{
-            //    void MyMethod()
-            //    {
-            //        int result = 2 + 2;
-            //        Console.WriteLine(result);
-            //    }
-            //}";
-            //        }
-
+        private static void Main(string[] args)
+        {
+            var obj1 = CSharpSyntaxTree.ParseText(tree1);
+            var objects = obj1.GetRoot().DescendantNodesAndSelf().ToArray();
         }
+    }
 }
